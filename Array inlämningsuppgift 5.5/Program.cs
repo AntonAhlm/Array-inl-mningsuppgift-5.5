1879665b77a21dd998ca2b17ae09f0a5056f9119
Anton <>
2023-03-31 09:22:54 +0200

    Add project files.

---
+++ Array inlämningsuppgift 5.5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] MagicSvar = { "Nej", "Ja", "Kanske", "Oklart", "Vet inte", "Absolut Inte!", "JAAA!!!", "Mycket Möjligt", "Tveksamt", "Fråga någon annan" };
+            string Svar = "";
+            Random Slump = new Random();
+
+            Console.WriteLine("Ställ en fråga : ");
+            Console.ReadLine();
+
+
+            while (Svar!="a")
+            {
+                
+                Console.WriteLine("Magic 8-ball tänker..");
+                Thread.Sleep(500);
+                Console.WriteLine("Svar : " + MagicSvar[Slump.Next(0,10)]);
+                Thread.Sleep(500);
+                Console.WriteLine("Ställ en fråga, eller skriv \"a\" för att avsluta : ");
+                Svar = Console.ReadLine();
+            }
+
+            
+            
+
+        }
+    }
+}
